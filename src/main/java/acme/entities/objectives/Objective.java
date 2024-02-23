de17efb7b268123d4de7178e6d944750c7350033
María C. Rodríguez <maromi0403@gmail.com>
2024-02-23 16:13:06 +0100

    Task 002 D02: Objective data model
    Created entity for objective and PriorityValue enum for one of the attributes.

---
+++ src/main/java/acme/entities/objectives/Objective.java
@@ -0,0 +1,62 @@
+
+package acme.entities.objectives;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Objective extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@PastOrPresent
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				instantiationMoment;
+
+	@NotBlank
+	@Length(max = 76)
+	private String				title;
+
+	@NotBlank
+	@Length(max = 101)
+	private String				description;
+
+	private PriorityValue		priority;
+
+	private Boolean				status;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				beginningMoment;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				finalMoment;
+
+	@URL
+	private String				optionalLink;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
---
+++ src/main/java/acme/entities/objectives/PriorityValue.java
@@ -0,0 +1,6 @@
+
+package acme.entities.objectives;
+
+public enum PriorityValue {
+	LOW, MEDIUM, HIGH
+}
