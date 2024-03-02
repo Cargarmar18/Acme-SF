2c3314841f32bf0cbe9c68548091e50552f1ddc0
Cargarmar18 <cargarmar18@alum.us.es>
2024-03-02 17:40:03 +0100

    Create Notice.java

---
+++ src/main/java/acme/entities/notice/Notice.java
@@ -0,0 +1,56 @@
+
+package acme.entities.notice;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Notice extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotNull
+	@PastOrPresent
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				moment;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				title;
+
+	//todo restriction
+	@NotBlank
+	@Length(max = 75)
+	private String				author;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				message;
+
+	@Email
+	private String				email;
+
+	@URL
+	private String				link;
+
+}
