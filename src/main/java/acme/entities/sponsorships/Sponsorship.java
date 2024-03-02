53e9d458770fb8243109291b1b844fbf521904ec
KSTY <mancasvel@icloud.com>
2024-03-02 21:34:22 +0100

    feat: Task 030 Student#4: invoices data model

--- src/main/java/acme/entities/sponsorships/Sponsorship.java
+++ src/main/java/acme/entities/sponsorships/Sponsorship.java
@@ -7,7 +7,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -64,8 +67,41 @@ public class Sponsorship extends AbstractEntity {
 	@URL
 	private String				moreInfo;
 
+	@Column(unique = true)
+	@NotBlank
+	@Pattern(regexp = "IN-[0-9]{4}-[0-9]{4}")
+	private String				invoiceCode;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@PastOrPresent
+	private Date				registrationTime;
+
+	// TO DO applying constraint of 1 month
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				dueDate;
+
+	// 	TO DO add constraint positive or 0 on money datatype
+	@NotNull
+	private Money				invoiceQuantity;
+
+	@Digits(integer = 3, fraction = 2)
+	@Min(0)
+	@Max(100)
+	private double				tax;
+
+	@URL
+	private String				InvoiceLink;
+
 	// Derived attributes -----------------------------------------------------
 
+
+	@Transient
+	public double totalAmount() {
+		return this.invoiceQuantity.getAmount() + this.tax * this.invoiceQuantity.getAmount() / 100;
+	}
+
 	// Relationships ----------------------------------------------------------
-	//relationship with project?
+	//TO DO relationship with project
 }
