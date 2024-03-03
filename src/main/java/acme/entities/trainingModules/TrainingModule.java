d0cc4ad410bd7be1ece76dd9107179906dfb7df1
María C. Rodríguez <maromi0403@gmail.com>
2024-03-03 14:38:04 +0100

    feat: Task 021 D02 Student#3: training module data model
    Implemented training module data model.

---
+++ src/main/java/acme/entities/trainingModules/TrainingModule.java
@@ -0,0 +1,61 @@
+
+package acme.entities.trainingModules;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class TrainingModule extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+	private static final long	serialVersionUID	= 1L;
+	// Attributes -------------------------------------------------------------
+
+	@Column(unique = true)
+	@NotBlank
+	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	private String				code;
+
+	@PastOrPresent
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	private Date				creationMoment;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				details;
+
+	@NotNull
+	private DifficultyLevel		difficultyLevel;
+
+	@PastOrPresent
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				updateMoment;
+
+	@URL
+	private String				link;
+
+	@Min(0)
+	private int					totalTime;
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+}
