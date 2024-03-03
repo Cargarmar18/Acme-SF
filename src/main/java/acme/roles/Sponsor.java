1bccbb03da92630d5daf1df630f34a9bea53f30b
KSTY <mancasvel@icloud.com>
2024-03-03 21:17:45 +0100

    feat: Task 033 Student#4: sponsor data model
    I have a couple questions about this implementation. The attribute benefits it is defined as a list but by the time it has this constraint "shorter than 101 characters" as a general benefits I saw it as a list written as a long string with the benefits. The other question is about the definition of sponsor which said that it was a role so I defined coming from Abstract role as the other roles.

---
+++ src/main/java/acme/roles/Sponsor.java
@@ -0,0 +1,44 @@
+
+package acme.roles;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Sponsor extends AbstractRole {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max = 75)
+	private String				name;
+
+	@NotBlank
+	@Length(max = 101)
+	private String				benefits;
+
+	@URL
+	private String				webPage;
+
+	@Email
+	private String				email;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
