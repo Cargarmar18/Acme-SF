141ee02958888e0fc3827a429c45648b4386b103
María C. Rodríguez <maromi0403@gmail.com>
2024-03-03 13:21:22 +0100

    fix: Task 002-D02 (R1)
    Fixed the errors in the Objective data model.

--- src/main/java/acme/entities/objectives/Objective.java
+++ src/main/java/acme/entities/objectives/Objective.java
@@ -6,7 +6,6 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
@@ -29,21 +28,23 @@ public class Objective extends AbstractEntity {
 
 	// Attributes -------------------------------------------------------------
 
+	@NotNull
 	@PastOrPresent
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date				instantiationMoment;
 
 	@NotBlank
-	@Length(max = 76)
+	@Length(max = 75)
 	private String				title;
 
 	@NotBlank
-	@Length(max = 101)
+	@Length(max = 100)
 	private String				description;
 
+	@NotNull
 	private PriorityValue		priority;
 
-	private Boolean				status;
+	private boolean				isCritical;
 
 	@NotNull
 	@Temporal(TemporalType.TIMESTAMP)
@@ -54,19 +55,9 @@ public class Objective extends AbstractEntity {
 	private Date				finalMoment;
 
 	@URL
-	private String				optionalLink;
+	private String				link;
 
 	// Derived attributes -----------------------------------------------------
-
-
-	@Transient
-	private Date executionPeriod() {
-		Date result;
-
-		result = new Date(this.finalMoment.getTime() - this.beginningMoment.getTime());
-
-		return result;
-	}
 
 	// Relationships ----------------------------------------------------------
 
