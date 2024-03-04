1f9ae91acb7f95843b16e9f954cf2b4518f1d763
Cargarmar18 <cargarmar18@alum.us.es>
2024-03-04 12:10:46 +0100

    create manager role

---
+++ src/main/java/acme/roles/Manager.java
@@ -0,0 +1,55 @@
+/*
+ * Consumer.java
+ *
+ * Copyright (C) 2012-2024 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.roles;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Manager extends AbstractRole {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max = 75)
+	private String				degree;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				overview;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				certifications;
+
+	@URL
+	private String				link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
