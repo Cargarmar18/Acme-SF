6e18f38ce25e13698c88ac4652baada643669ae0
María C. Rodríguez <maromi0403@gmail.com>
2024-03-04 16:42:23 +0100

    feat: Task 022 D02 Student#3: Training sessions data model
    Added the training sessions data model.

---
+++ src/main/java/acme/entities/trainingSessions/TrainingSession.java
@@ -0,0 +1,61 @@
+
+package acme.entities.trainingSessions;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.entities.trainingModules.TrainingModule;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class TrainingSession {
+
+	// Serialisation identifier -----------------------------------------------
+	private static final long	serialVersionUID	= 1L;
+	// Attributes -------------------------------------------------------------
+
+	@Column(unique = true)
+	@NotBlank
+	@Pattern(regexp = "TS-[A-Z]{1,3}-[0-9]{3}")
+	private String				code;
+
+	@NotNull
+	@Min(1)
+	private int					timePeriod;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				location;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				instructor;
+
+	@NotNull
+	@Email
+	private String				contactEmail;
+
+	@URL
+	private String				link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private TrainingModule		trainingModule;
+}
