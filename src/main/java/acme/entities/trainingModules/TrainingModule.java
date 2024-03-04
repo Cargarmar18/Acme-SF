d90ac5e13678b58128c43e16cbc90e5fc9fbb978
María C. Rodríguez <maromi0403@gmail.com>
2024-03-04 16:01:03 +0100

    fix: Task 021 D02: relationship with project
    Added the relationship with project in the training module data model.

--- src/main/java/acme/entities/trainingModules/TrainingModule.java
+++ src/main/java/acme/entities/trainingModules/TrainingModule.java
@@ -5,8 +5,10 @@ import java.sql.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -17,6 +19,7 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
+import acme.entities.project.Project;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -58,4 +61,9 @@ public class TrainingModule extends AbstractEntity {
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
+	@NotNull
+	@Valid
+	@ManyToOne
+	private Project				project;
+
 }
