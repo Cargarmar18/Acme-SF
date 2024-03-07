ced93251a58c74497d6c7490a507f4b0d551f659
KSTY <mancasvel@icloud.com>
2024-03-07 18:20:28 +0100

    fix: data.csv working

--- src/main/java/acme/entities/sponsorships/Sponsorship.java
+++ src/main/java/acme/entities/sponsorships/Sponsorship.java
@@ -10,7 +10,6 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
@@ -48,15 +47,14 @@ public class Sponsorship extends AbstractEntity {
 
 	@NotNull
 	@Temporal(TemporalType.TIMESTAMP)
-	@PastOrPresent
 	private Date				startSponsor;
 
 	@NotNull
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date				endSponsor;
 
+	//to do constraint that can not be smaller than 0
 	@NotNull
-	@Min(0)
 	private Money				amount;
 
 	@NotNull
