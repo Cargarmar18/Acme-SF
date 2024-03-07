059e525ef0d57ed4148b99ab571fbb3473a0eea5
María C. Rodríguez <maromi0403@gmail.com>
2024-03-07 17:05:35 +0100

    fix: Add getters and setters
    Added forgotten setters and getters.

--- src/main/java/acme/forms/DeveloperDashboard.java
+++ src/main/java/acme/forms/DeveloperDashboard.java
@@ -2,7 +2,11 @@
 package acme.forms;
 
 import acme.client.data.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class DeveloperDashboard extends AbstractForm {
 
 	// Serialisation identifier -----------------------------------------------
