8b04c5641708d60b8decfc65814d1cb287390563
María C. Rodríguez <maromi0403@gmail.com>
2024-03-09 13:38:26 +0100

    fix: @Pattern failures
    Fixed the code errors.

--- src/main/java/acme/entities/trainingModules/TrainingModule.java
+++ src/main/java/acme/entities/trainingModules/TrainingModule.java
@@ -35,7 +35,7 @@ public class TrainingModule extends AbstractEntity {
 
 	@Column(unique = true)
 	@NotBlank
-	@Pattern(regexp = "^[A-Z]{1,3}-[0-9]{3}$")
+	@Pattern(regexp = "^[A-Z]{1,3}-[0-9]{3}$", message = "{validation.trainingModule.reference}")
 	private String				code;
 
 	@NotNull
