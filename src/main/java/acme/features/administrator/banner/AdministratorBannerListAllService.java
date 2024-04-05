5225179b4af065d8883945f53f0de3db3c23afe9
KSTY <mancasvel@icloud.com>
2024-04-05 12:58:20 +0200

    feat: List All.

--- src/main/java/acme/features/administrator/banner/AdministratorBannerListAllService.java
+++ src/main/java/acme/features/administrator/banner/AdministratorBannerListAllService.java
@@ -1,6 +1,48 @@
+
 package acme.features.administrator.banner;
 
+import java.util.Collection;
 
-public class AdministratorBannerListAllService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.accounts.Administrator;
+import acme.client.data.models.Dataset;
+import acme.client.services.AbstractService;
+import acme.entities.banner.Banner;
+
+@Service
+public class AdministratorBannerListAllService extends AbstractService<Administrator, Banner> {
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AdministratorBannerRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<Banner> objects;
+
+		objects = this.repository.findAllBanner();
+
+		super.getBuffer().addData(objects);
+	}
+	@Override
+	public void unbind(final Banner object) {
+		assert object != null;
+
+		Dataset dataset;
+
+		dataset = super.unbind(object, "instatiationUpdateMoment", "startDisplay", "endDisplay", "pictureLink", "slogan", "targetLink");
+
+		super.getResponse().addData(dataset);
+	}
 
 }
--- src/main/java/acme/features/administrator/banner/AdministratorBannerRepository.java
+++ src/main/java/acme/features/administrator/banner/AdministratorBannerRepository.java
@@ -1,13 +1,18 @@
 
 package acme.features.administrator.banner;
 
-public class AdministratorBannerRepository {
-	/*
-	 * @Query("select e from Employer e where e.userAccount.id = :id")
-	 * Employer findOneEmployerByUserAccountId(int id);
-	 * 
-	 * @Query("select ua from UserAccount ua where ua.id = :id")
-	 * UserAccount findOneUserAccountById(int id);
-	 * 
-	 */
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.banner.Banner;
+
+@Repository
+public interface AdministratorBannerRepository extends AbstractRepository {
+
+	@Query("select b from Banner b")
+	Collection<Banner> findAllBanner();
+
 }
