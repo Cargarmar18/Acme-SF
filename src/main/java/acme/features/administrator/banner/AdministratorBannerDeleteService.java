57c0c93a59e9833cb2674ac7757d38a815ddaa5c
KSTY <mancasvel@icloud.com>
2024-04-29 05:50:00 +0200

    fix: bean

--- src/main/java/acme/features/administrator/banner/AdministratorBannerDeleteService.java
+++ src/main/java/acme/features/administrator/banner/AdministratorBannerDeleteService.java
@@ -2,12 +2,14 @@
 package acme.features.administrator.banner;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import acme.client.data.accounts.Administrator;
 import acme.client.data.models.Dataset;
 import acme.client.services.AbstractService;
 import acme.entities.banner.Banner;
 
+@Service
 public class AdministratorBannerDeleteService extends AbstractService<Administrator, Banner> {
 	// Internal state ---------------------------------------------------------
 
